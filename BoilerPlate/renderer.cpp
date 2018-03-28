17a7b61a7e2cff0cefa6421764db0c9b79002ad8
Adolfo <adolfoaguirrecontreras@gmail.com>
2018-03-28 13:57:23 -0400

    fixed renderization issues

--- BoilerPlate/renderer.cpp
+++ BoilerPlate/renderer.cpp
@@ -5,16 +5,18 @@ namespace engine {
 
 
 	float vertices[] = {
-		// positions          // colors           // texture coords
+		// positions         // colors          // texture coords
 		0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
 		0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
 		-0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
 		-0.5f,  0.5f, 0.0f,  1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
+
 	};
 
 	unsigned int indices[] = {  // note that we start from 0!
 		0, 1, 3,  // first Triangle
-		1, 2, 3   // second Triangle
+		1, 2, 3 // second Triangle
+	
 	};
 
 	renderer::renderer() {
